cc6411da54815d042b5a9d65890761739ce11559
akshayaravindan96 <akshayaravindan96@users.noreply.github.com>
2019-08-01 09:26:01 -0400

    Added space bar to start motion

--- Assets/Scripts/MoveonPathScript.cs
+++ Assets/Scripts/MoveonPathScript.cs
@@ -14,11 +14,13 @@ public class MoveonPathScript : MonoBehaviour {
     public string pathName;
     public float str = 10f;
     public Collider tote;
+    public Collider blue_box;
     public Camera front;
     public Camera back;
     public Camera rightside;
     public Camera leftside;
     public Camera top;
+    private int start = 0;
 
     Vector3 last_position;
     Vector3 current_position;
@@ -27,91 +29,101 @@ public class MoveonPathScript : MonoBehaviour {
 	void Start () {
         //PathToFollow = GameObject.Find(pathName).GetComponent<EditorPathScript> ();
         last_position = transform.position;
-	}
-	
-	
-	void Update () {
-        //Camera toggling
-        if(Input.GetKeyDown("1"))
-        {
-            front.enabled = true;
-            back.enabled = false;
-            rightside.enabled = false;
-            leftside.enabled = false;
-            top.enabled = false;
-        }
-        if (Input.GetKeyDown("2"))
-        {
-            front.enabled = false;
-            back.enabled = true;
-            rightside.enabled = false;
-            leftside.enabled = false;
-            top.enabled = false;
-        }
-        if (Input.GetKeyDown("3"))
-        {
-            front.enabled = false;
-            back.enabled = false;
-            rightside.enabled = true;
-            leftside.enabled = false;
-            top.enabled = false;
-        }
-        if (Input.GetKeyDown("4"))
-        {
-            front.enabled = false;
-            back.enabled = false;
-            rightside.enabled = false;
-            leftside.enabled = true;
-            top.enabled = false;
-        }
-        if (Input.GetKeyDown("5"))
-        {
-            front.enabled = false;
-            back.enabled = false;
-            rightside.enabled = false;
-            leftside.enabled = false;
-            top.enabled = true;
-        }
-
-        if (CurrentWayPointID == 3)
-            speed = 3;
-        float distance = Vector3.Distance(PathToFollow.path_objs[CurrentWayPointID].position, transform.position);
-        transform.position = Vector3.MoveTowards(transform.position, PathToFollow.path_objs[CurrentWayPointID].position, Time.deltaTime*speed);
-
-        var rotation = Quaternion.LookRotation(PathToFollow.path_objs[CurrentWayPointID].position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-        if(distance <= reachDistance)
-        {
-            CurrentWayPointID++;
-        }
         
-        if(CurrentWayPointID >= PathToFollow.path_objs.Count)
-        {
-            //CurrentWayPointID = 0;
-        }
-        if(flag!=0)
-        {
-            Rigidbody body = tote.attachedRigidbody;
-            tote_position = new Vector3(transform.position.x , transform.position.y + 2.2f, transform.position.z+ 0.2f);
-            tote.gameObject.transform.position = Vector3.MoveTowards(tote.gameObject.transform.position, tote_position, 2);
-            
-            tote.gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-        }
-        if(CurrentWayPointID==10)
-        {
-            speed = 1;
-        }
-        if(CurrentWayPointID==11)
-        {
+    }
 
-            tote.gameObject.SetActive(false);
-            
-        }
-        if(CurrentWayPointID==13)
+
+    void Update()
+    {
+        //Starting AGV motion
+        if (Input.GetKeyDown("space"))
         {
-            speed = 3;
+            start = 1;
         }
-	}
+        if (start == 1)
+        {
+            //Camera toggling
+            if (Input.GetKeyDown("1"))
+            {
+                front.enabled = true;
+                back.enabled = false;
+                rightside.enabled = false;
+                leftside.enabled = false;
+                top.enabled = false;
+            }
+            if (Input.GetKeyDown("2"))
+            {
+                front.enabled = false;
+                back.enabled = true;
+                rightside.enabled = false;
+                leftside.enabled = false;
+                top.enabled = false;
+            }
+            if (Input.GetKeyDown("3"))
+            {
+                front.enabled = false;
+                back.enabled = false;
+                rightside.enabled = true;
+                leftside.enabled = false;
+                top.enabled = false;
+            }
+            if (Input.GetKeyDown("4"))
+            {
+                front.enabled = false;
+                back.enabled = false;
+                rightside.enabled = false;
+                leftside.enabled = true;
+                top.enabled = false;
+            }
+            if (Input.GetKeyDown("5"))
+            {
+                front.enabled = false;
+                back.enabled = false;
+                rightside.enabled = false;
+                leftside.enabled = false;
+                top.enabled = true;
+            }
+
+            if (CurrentWayPointID == 3)
+                speed = 3;
+            float distance = Vector3.Distance(PathToFollow.path_objs[CurrentWayPointID].position, transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, PathToFollow.path_objs[CurrentWayPointID].position, Time.deltaTime * speed);
+
+            var rotation = Quaternion.LookRotation(PathToFollow.path_objs[CurrentWayPointID].position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+            if (distance <= reachDistance)
+            {
+                CurrentWayPointID++;
+            }
+
+            if (CurrentWayPointID >= PathToFollow.path_objs.Count)
+            {
+                //CurrentWayPointID = 0;
+            }
+            if (flag != 0)
+            {
+                Rigidbody body = tote.attachedRigidbody;
+                tote_position = new Vector3(transform.position.x, transform.position.y + 2.2f, transform.position.z + 0.2f);
+                tote.gameObject.transform.position = Vector3.MoveTowards(tote.gameObject.transform.position, tote_position, 2);
+
+                tote.gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+            }
+            if (CurrentWayPointID == 10)
+            {
+                speed = 1;
+            }
+            if (CurrentWayPointID == 11)
+            {
+
+                blue_box.gameObject.SetActive(false);
+
+            }
+            if (CurrentWayPointID == 13)
+            {
+                speed = 3;
+            }
+        }
+    }
 
 
 
